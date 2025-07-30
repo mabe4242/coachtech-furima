79d74a8606bf2c995a1be0da52fcb0f309b6b26b
mabe4242 <kouiy42@gmail.com>
2025-07-30 15:05:28 +0900

    userのダミーデータをシーディングで作成

---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'テストユーザー',
+            'email' => 'test@example.com',
+            'password' => Hash::make('11111111'),
+            'email_verified_at' => Carbon::now(),
+            'remember_token' => Str::random(10),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
+
